974156dc5402e3dddd7427f520c6a4a6fd175ee1
Turgay-Kara <84818047+Turgay-Kara@users.noreply.github.com>
2021-08-13 11:36:09 +0300

    Update Adam Asmaca.py

--- Adam Asmaca.py
+++ Adam Asmaca.py
@@ -1,19 +1,19 @@
 import random
-import sys #-> Oyun kazanılırsa oyundan atılmak için kullanılır.
-from time import sleep #-> Kuralları anlatırken beklet(sırayla çıkart).
+import sys
+from time import sleep
 
 Kelime_Listesi = ["elma", "armut", "muz", "kivi", "kitap", "kalem", "defter", "silgi", "köpek", "kedi", "yılan", "kartal", "monitör", "klavye", "fare", "hoparlör"]
-TahminEdilen_Kelime = []    #-> Oyunda tahmin edilen kelime her seferinde farklı olmasını istediğimiz için içi boş bir liste olacak. []
+TahminEdilen_Kelime = []
 Gizli_Kelime = random.choice(Kelime_Listesi)
-Kelime_Uzunluğu = len(Gizli_Kelime) #-> Kelimenin kaç harf olduğun görmek için kullandık. len Herhangi bir stringdeki karakter sayısını bize int olarak veren bir modül.
-Alfabe = "abcdefghijklmnopqrstuvwxyz" #-> ! gibi işaretlerin kullanılmamasını istediğimiz için sadece alfabeyi yazdık.
-Harf_Deposu = [] #-> Bir defa tahmin ettiğimiz harfi tekrar tahmin etmemek için kullandık. Tahmin edilen harfler Harf_Deposuna gider.
+Kelime_Uzunluğu = len(Gizli_Kelime)
+Alfabe = "abcdefghijklmnopqrstuvwxyz"
+Harf_Deposu = []
 
 def kurallar():
     for karakter in Gizli_Kelime:
-        TahminEdilen_Kelime.append("-") #->Gizli_Kelimeyi Hanelere böldük. Kelimedeki her harf yerine - koyar.
+        TahminEdilen_Kelime.append("-")
     print("Tahmin  etmemiz gereken kelimede ", Kelime_Uzunluğu, " Harf var.")
-    sleep(1) #-> Kuralları 1 saniye arayla anlat.
+    sleep(1)
     print("10 tahmin hakkınız var. ")
     print("10 seferde bilemezseniz oyunu kaybedersiniz!")
     print(TahminEdilen_Kelime)
@@ -36,34 +36,34 @@ def Kelime_TahminEtme():
             break
         else:
             print("Lütfen evet veya hayır diye bir cevap veriniz.\n")
-            continue #-> if komutunun en başına gelir.
+            continue
 
 def Harf_TahminEtme():
     TahminSayısı = 0
 
-    while TahminSayısı < 10: #-> Oyuncu oyunu kaybetmemişken..
+    while TahminSayısı < 10:
         Kelime_TahminEtme()
         Harf_Deneme = input("Bir harf deneyin.\n ")
-        if not Harf_Deneme in Alfabe:   #-> Eğer Harf_Deneme albadede değil ise.
+        if not Harf_Deneme in Alfabe:
             print("Lütfen alfabeden bir karakter yazınız.\n ")
         elif Harf_Deneme in Harf_Deposu:
             print("Bu harfi zaten tahmin ettiniz.\n ")
         else:
-            Harf_Deposu.append(Harf_Deneme) #-> .append Harf_Deneme, Harf_Deposu nda yoksa bu harfi Harf_Deposu na ekler.
+            Harf_Deposu.append(Harf_Deneme) 
 
         if Harf_Deneme in Gizli_Kelime:
             print("Doğru Tahmin!")
-            for x in range(0, Kelime_Uzunluğu):   #-> for loop'u Gizli_Kelimemiz içinde Kelime_Uzunluğu kadar gidip gelicek  #-> for loopu belirli bir sayıda tekrar yapmak iin kullanılır.
-                if Gizli_Kelime[x] == Harf_Deneme:  #-> index method, [x]-> x index'i ->  for loop'unun Gizli_Kelimenin içinde gezerken bulduğu o kelimenin index'i
-                    TahminEdilen_Kelime[x] = Harf_Deneme #-> Tahmin edilen kelimenin x'inci sıradaki(elementindei?) -'yi Harf_Deneme'sine değiştiriyoruz.
+            for x in range(0, Kelime_Uzunluğu):
+                if Gizli_Kelime[x] == Harf_Deneme:
+                    TahminEdilen_Kelime[x] = Harf_Deneme
                     print(TahminEdilen_Kelime)
-                if not "-" in TahminEdilen_Kelime: #-> Eğer oyuncunun son tahmini ise ve kelimeyi buldu ise. Listede - kalmamasını farkeden bir komut yazdık.
+                if not "-" in TahminEdilen_Kelime:
                     print("Tebrikler, Oyunu kazandınız!")
                     break
         else:
-            print("Yanlış tahmin. Tekrar deneyin.\n")   #-> Eğer harfi yanlış tahmin ederse.
-            TahminSayısı += 1   #-> Tahmin sayısına 1 ekler.
-        if TahminSayısı == 10: #-> Tahmin sayısı 10'a ulaşınca.
+            print("Yanlış tahmin. Tekrar deneyin.\n")
+            TahminSayısı += 1
+        if TahminSayısı == 10:
             print("Kaybettiniz! Gizli kelime:\n ", Gizli_Kelime)
 
 kurallar()
