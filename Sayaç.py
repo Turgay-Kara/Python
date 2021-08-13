7708c958128646aeba6b36d6b501aaf2a51d5e35
Turgay-Kara <84818047+Turgay-Kara@users.noreply.github.com>
2021-08-13 11:19:33 +0300

    Add files via upload

---
+++ Sayaç.py
@@ -0,0 +1,10 @@
+sayaç = 10
+while sayaç >= 0:
+    print("Sayaç = " + str(sayaç))
+    sayaç = sayaç -1
+print("Sayaç bitti.")
+print("Sayaç -'ye doğru artıyor.")
+while sayaç >= -10:
+    print("Sayaç = " + str(sayaç))
+    sayaç = sayaç -1
+print("Sayaç işlemini durdurdu!!")
