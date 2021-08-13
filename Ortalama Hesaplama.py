fdd7e1c3ae1b5561641d2c8dfaa4be3eebc8e131
Turgay-Kara <84818047+Turgay-Kara@users.noreply.github.com>
2021-08-13 11:20:34 +0300

    Add files via upload

---
+++ Ortalama Hesaplama.py
@@ -0,0 +1,5 @@
+not1 = int(input("İlk sınav notunu girin\n"))
+not2 = int(input("ikinci sınav notunu girin\n"))
+not3 = int(input("Üçüncü sınav notunu girin\n"))
+ortalama = (not1 + not2 + not3)/3
+print("Ortalama:" + str(ortalama))
