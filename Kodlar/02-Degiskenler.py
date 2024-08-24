166477603da969b67bd4cf308bc541d21d9dfdbc
Turgay KARA <84818047+Turgay-Kara@users.noreply.github.com>
2024-08-24 17:53:04 +0300

    Create 02-Degiskenler.py

---
+++ Kodlar/02-Degiskenler.py
@@ -0,0 +1,23 @@
+    #DEĞİŞKENLER ve SABİTLER (Variables and Constants)
+
+ #değişkenler;
+#a = 'turgay' #değişkenin ismi a, değeri turgay
+#b = 1.34
+#print(b + 3) #- 4.34
+
+
+#isim, isim2 = turgay, talha
+#print(isim, isim2)  #- turgay talha
+
+
+ #sabitler;
+#PI = 3.14 #- Sabitler değiştirilemez ve Değişkenin ismi BÜYÜK harfle yazılır.
+
+
+
+
+    #NUMBERS
+ #INTEGER(int)  #- Tam Sayılar (-, 0, +)
+#x = 34
+#y = 0
+#z = -34
