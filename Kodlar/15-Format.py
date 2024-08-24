50442629cd3ab1774306b5fc69d438155e949b42
Turgay KARA <84818047+Turgay-Kara@users.noreply.github.com>
2024-08-24 17:54:14 +0300

    Create 15-Format.py

---
+++ Kodlar/15-Format.py
@@ -0,0 +1,59 @@
+    #FORMAT
+#isim = "Turgay"
+#soyisim = "Kara"
+#print(f"{isim} {soyisim} bir insandir.")
+
+
+#isim = "Turgay"
+#soyisim = "Kara"
+#print("Benim ismim {} soyadim {} ".format(isim, soyisim))     #-> .format() süslü parantezin içine istenileni yazar.
+
+
+#isim = "Turgay"
+#soyisim = "Kara"
+#print("{0} {1} bir  insandir.".format(isim, soyisim))
+
+
+#isim = "Turgay"
+#soyisim = "Kara"
+#print("{C1} {C2} bir  insandir.".format(C1 = isim, C2 = soyisim))
+
+
+#a=10
+#b=15
+#print("{} > {}".format(a,b), a < b)    #-> True
+#print("{} > {}".format(b,a), b < a)    #-> False
+
+
+ #format örnekleri
+"""
+name = "Turgay"
+surname = "Kara"
+age = 20
+job = "Full-Stack deweloper"
+city = "İstanbul"
+salery = 12000
+print(f"Ben {name} {surname}\n{age} yaşimdayim.\n{city} şehrinde yaşiyorum.\nAyda {salery} dolar kazaniyorum.")
+"""
+
+#numberone = 20
+#numbertwo = 12
+#print(f"Toplam : {numberone + numbertwo}\nÇikartma : {numberone - numbertwo}\nÇarpma : {numberone * numbertwo}\nBölme : {numberone / numbertwo}")
+
+
+
+    #RETURN
+  #Bir sonuç döndürmesi istenen fonksiyonlarda fonksiyonun çaliştiktan sonra bir değer döndürmesi için return anahtar kelimesi kullanilir.
+"""
+def alan(r, pi = 3.14):
+    return pi*r**2
+alan1 = alan(6)
+alan2 = alan(8)
+print(alan1 + alan2)
+"""
+
+#def asdasd():
+    #pass
+
+#if 3 > 1:
+    #pass    #-> Bu satirda Sonradan bir düzeltme yapacaksaniz pass komutu işinize yarayacaktir.
