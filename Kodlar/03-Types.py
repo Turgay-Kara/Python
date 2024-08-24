1acb04f466303682f76e80c057ca1d828a59a50a
Turgay KARA <84818047+Turgay-Kara@users.noreply.github.com>
2024-08-24 17:53:09 +0300

    Create 03-Types.py

---
+++ Kodlar/03-Types.py
@@ -0,0 +1,38 @@
+    #FLOAT(float) #-> Noktali Sayilar (2.34, -0.40)
+"""
+ortalama = 95.7
+sicaklik = -3
+para = -0.31
+
+x = para 
+print(type(sicaklik))  #-> Değişkenin tipini gösterir. -> 'int'
+print(ortalama)        #-> Değerini gösterir. -> 95.7  
+print(type(x))         #-> x'in yani para'nin tipini gösterir. -> 'float'
+                       #-> Noktali sayi olduğu için 'float'
+"""
+
+
+    #STRİNG(str) #-> "çift tirnak" ("123", "1.23", "TURGAY", "A")
+#isim = "Turgay Kara"
+#şifre = "şifre123e"
+#yaş = "16" #->INTEGER DEĞİL BU BİR STRİNG ->çünkü " içine alinmiş.
+#print(type(yaş))    #-> 'str'
+#print(isim[3]) #-> 3.karakteri yazar -> "g"
+
+
+    #BOOLEAN
+  #Boolean (bool) #-> 2 değeri var. True ve False
+"""
+havagüzel = False #-> Hava güzel değil ise
+yağmuryağiyor = True
+
+print(havagüzel)    #-> False
+print(type(havagüzel)) #->Değişken tipini gösterir. 'bool'
+print(yağmuryağiyor) #-> True
+"""
+
+
+# []  -> List (liste)  / Değitirilebilir.
+# ()  -> Tuple (demet) / Değiştirilemez
+# ()  -> Set (küme)
+# {}  -> Dictionary (sözlük)
